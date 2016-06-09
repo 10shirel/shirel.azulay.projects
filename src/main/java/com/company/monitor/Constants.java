6e2bb5fc0672cfff8d2694d54f6321b06004c166
Shirela <shirel.azulay@redbend.com>
2016-06-09 17:07:26 +0300

    New project

---
+++ src/main/java/com/company/monitor/Constants.java
@@ -0,0 +1,20 @@
+package com.company.monitor;
+
+public final class Constants {
+
+  public static final String TXT_URL_FILE = "files.txt";
+  public static final String TXT_SHA1_TO_URL_FILE = "urlToSha1ConcurrentMapSavedToFile.txt";
+  public static final String EQUALS = "=";
+  public static final String EMPTY_STRING = "";
+  public static final String URL = "url_";
+  public static final String SHA1 = "sha1_";
+  public static final String UTF8 = "UTF-8";
+  public static final String FIRST_ITERATION = "isFirstIteration=1";
+  public static final String NOT_FIRST_ITERATION = "isFirstIteration=0";
+  public static final String IS_FIRST_ITERATION_KEY = "isFirstIteration";
+  public static final String IS_FIRST_ITERATION_VALUE_TRUE = "1";
+  public static final String IS_FIRST_ITERATION_VALUE_FALSE = "0";
+
+
+
+}
